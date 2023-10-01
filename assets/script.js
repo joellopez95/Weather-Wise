c7b846599b80694524dcde9776a42c61cb56df4e
Joel Lopez <jolopez.satx@gmail.com>
2023-10-01 17:14:12 -0500

    origin-main

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,30 @@
 var cityName = 'San Antonio';
+var apiKey = 'e38952739d3c9739605b17f167baf501'
+var cityForm = document.querySelector('#city-form');
+var cityInput = document.querySelector('#city-input');
+var searchHistory = document.querySelector('#search-history');
+var currentWeather = document.querySelector('#current-weather');
+var forecast = document.querySelector('#forecast');
+
 
 
 //GIVEN a weather dashboard with form inputs
+//function to create current weather 
 
+function displayCurrentWeather(data){
+var city = data.name;
+//had to convert timestamp to date (stackoverflow)
+var date = new Date(data.dt * 1000);
+var icon = data.weather[0].icon;
+var temperature = data.main.temp;
+var humidity = data.main.humidity;
+var windSpeed = data.wind.speed;
+
+
+}
 //WHEN I search for a city
 var apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}';
+
 //obtain api using fetch (same ex as mini proj)
 fetch(apiURL)
 .then(function(response){
@@ -17,16 +37,26 @@ fetch(apiURL)
 })
 .then(function(data){
     console.log(data)
-    if (!data.coord || !data.coord.lat || !data.coord.lon){
-        console.error('Error: Unable to locate coord for city');  
+    if (data.coord || data.coord.lat || data.coord.lon){
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+    } else{
+        console.error('Error: Coordinates not found for the city.');
     }
 // Extract the latitude and longitude from the response
-    var lat = data.coord.lat;
-    var lon = data.coord.lon;
+
     var fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
 
     fetch(fiveDayURL)
-
+        .then(function (response){
+            if(!response.ok) {
+                throw new Error('Network not ok');
+            }
+            return response.json();
+        })
+        .then(function (weatherData){
+            console.log(weatherData);
+        })
 })
 .catch(function(error){
     console.error(error);
